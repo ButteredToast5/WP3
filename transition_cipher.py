936a88a6310078332f7f6fd41cf7d835210ee511
Juliet Wang <166184712+ButteredToast5@users.noreply.github.com>
2026-02-23 20:06:34 -0800

    Add transposition cipher decryption

--- transition_cipher.py
+++ transition_cipher.py
@@ -27,3 +27,38 @@ def encrypt(plaintext: str, key: int) -> str:
             pointer += key
 
     return "".join(columns)
+
+def decrypt(ciphertext: str, key: int) -> str:
+    """
+    Decrypt ciphertext encrypted with transposition cipher.
+    """
+
+    if key < 2 or key > len(ciphertext):
+        raise ValueError("Invalid key.")
+
+    # Determine grid dimensions
+    num_cols = math.ceil(len(ciphertext) / key)
+    num_rows = key
+    num_shaded = (num_cols * num_rows) - len(ciphertext)
+
+    # Prepare storage for reconstructed columns
+    plain_cols = [""] * num_cols
+
+    col = 0
+    row = 0
+
+    for symbol in ciphertext:
+        plain_cols[col] += symbol
+        col += 1
+
+        if (
+            col == num_cols
+            or (
+                col == num_cols - 1
+                and row >= num_rows - num_shaded
+            )
+        ):
+            col = 0
+            row += 1
+
+    return "".join(plain_cols)
