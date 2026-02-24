332bc13b5023d3c2b3d29e1f6e8e212ef121cfed
Juliet Wang <166184712+ButteredToast5@users.noreply.github.com>
2026-02-23 21:19:25 -0800

    added docustrings

--- key_utilities.py
+++ key_utilities.py
@@ -2,10 +2,20 @@
 # Julietw2@uci.edu
 # 65183805
 
+"""
+key_utils.py
+
+Contains helper functions for validating and generating
+keys used in the transposition cipher.
+"""
+
 from wp3_exceptions import InvalidKeyError
 
 
 def validate_key(key: int, text_length: int) -> None:
+    """
+    Validates that a key is usable for the transposition cipher.
+    """
     if not isinstance(key, int):
         raise InvalidKeyError("Key must be integer.")
 
@@ -14,5 +24,8 @@ def validate_key(key: int, text_length: int) -> None:
 
 
 def candidate_keys(text_length: int):
+    """
+    Generates all possible keys for brute-force search.
+    """
     for k in range(2, text_length + 1):
         yield k
